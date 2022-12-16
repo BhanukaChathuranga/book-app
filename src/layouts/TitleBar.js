170bc60740cce6b5afa93e362bb628b745b84e1d
bhanukac-xino <bhanukac@xinotechsoftware.com>
2022-12-17 00:25:46 +0530

    fix: remove notification icon

--- src/layouts/TitleBar.js
+++ src/layouts/TitleBar.js
@@ -1,24 +1,23 @@
 import useDarkMode from 'hooks/useDarkMode';
 import {
-  FaRegBell,
+  // FaRegBell,
   FaMoon,
   FaSun,
 } from 'react-icons/fa';
 
 function TitleBar() {
   return (
-    // <div className="top-0 ml-16 h-16 w-full bg-gray-600 text-blue-500 flex justify-end shadow-lg">
     <div className="top-navigation">
        <Title />
       <ThemeIcon />
-      <BellIcon />
+      {/* <BellIcon /> */}
     </div>
   );
 }
 
 const Title = () => <h5 className="title-text">Book Shop</h5>;
 
-const BellIcon = () => <FaRegBell size="24" className="top-navigation-icon" data-modal-toggle="authentication-modal"/>;
+// const BellIcon = () => <FaRegBell size="24" className="top-navigation-icon" data-modal-toggle="authentication-modal"/>;
 
 const ThemeIcon = () => {
   const [darkTheme, setDarkTheme] = useDarkMode();
